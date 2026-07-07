45b8b81282d9df3512d8cf912c4b7b527fb382ce
sweettrain-ggul <sweettrain-ggul@users.noreply.github.com>
2026-07-07 21:56:34 +0900

    clean generated files

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-export default defineConfig({
-    base: "/ddan_travel/",
-    server: {
-        host: true,
-    },
-    plugins: [react()],
-});
